17fc0f1e65d97b73b746b4ec47c87658485216a6
Steven Williams <evilgeniusnyc@me.com>
2020-02-28 17:40:48 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,4 @@
+def greeting
+  puts "Hello World"
+end
+ruby greeting.#!/usr/bin/env ruby -wKU
--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,19 @@
-# Use this space to code along with the readme
+authenticate_or_request_with_http_basic# Use this space to code along with the readme
+def say_hello_world_ten_times
+  phrase = "Hello World!
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
 
 phrase = "Hello World!"
 puts phrase
